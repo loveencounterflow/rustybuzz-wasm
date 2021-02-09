f0359a1999c9e9427cff393a79b25cbba98b6c35
flow <flow@enceladus.orbit>
2021-02-09 16:36:09 +0100

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use wasm_bindgen::prelude::*;
+// mod shape;
 
 #[wasm_bindgen]
 extern {
@@ -9,3 +10,9 @@ extern {
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
 }
+
+// #[wasm_bindgen]
+// pub fn main( text: str ) -> std::string::String {
+//   // shape::shape_text();
+//   return format!( "Hello, {}!", text );
+// }
