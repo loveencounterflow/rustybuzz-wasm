be017108e003ea74b37d17d08e7803c3a647ab90
flow <flow@enceladus.orbit>
2021-02-09 22:06:12 +0100

    update

--- demo-nodejs-using-wasm/lib/main.js
+++ demo-nodejs-using-wasm/lib/main.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  var CND, alert, badge, debug, echo, help, info, rpr, urge, warn, whisper;
+  var CND, FS, PATH, alert, badge, debug, echo, help, info, rpr, urge, warn, whisper;
 
   //###########################################################################################################
   CND = require('cnd');
@@ -25,21 +25,43 @@
 
   echo = CND.echo.bind(CND);
 
+  FS = (require('fs')).promises;
+
+  PATH = require('path');
+
+  // { promisify }
+
+  //-----------------------------------------------------------------------------------------------------------
+  this.load_wasm = async function(path) {
+    var data, env, result, typedArray;
+    env = {
+      memoryBase: 0,
+      tableBase: 0,
+      memory: new WebAssembly.Memory({
+        initial: 256
+      }),
+      table: new WebAssembly.Table({
+        initial: 0,
+        element: 'anyfunc'
+      })
+    };
+    data = (await FS.readFile(path));
+    typedArray = new Uint8Array(data);
+    result = (await WebAssembly.instantiate(typedArray)); //, env.memory
+    // result      = await WebAssembly.instantiate typedArray, { env, }
+    // result  = await WebAssembly.instantiateStreaming data
+    result.instance.exports.memory.grow(400); // each page is 64kb in size
+    return result.instance;
+  };
+
   //###########################################################################################################
   if (module === require.main) {
-    (async() => {
-      var error, i, len, path, paths;
-      paths = ['../../pkg/hello_wasm.js', '../../pkg/hello_wasm_bg.js', '../../pkg/hello_wasm_bg.wasm', '../../pkg/package.json', '@loveencounterflow/hello-wasm'];
-      for (i = 0, len = paths.length; i < len; i++) {
-        path = paths[i];
-        whisper(path);
-        try {
-          debug((await require(path)));
-        } catch (error1) {
-          error = error1;
-          warn(error.message);
-        }
-      }
+    (() => {
+      /* NOTE only works with `wasm-pack build --target nodejs` */
+      var HELO;
+      globalThis.alert = alert;
+      HELO = require('../../pkg');
+      HELO.greet('everyone');
       return null;
     })();
   }
