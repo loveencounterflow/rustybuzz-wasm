cf29960152f26d6105d0fa4db6f6c486379aee19
flow <flow@enceladus.orbit>
2021-02-09 22:18:52 +0100

    remove old code

--- demo-nodejs-using-wasm/lib/main.js
+++ demo-nodejs-using-wasm/lib/main.js
@@ -31,29 +31,6 @@
 
   // { promisify }
 
-  //-----------------------------------------------------------------------------------------------------------
-  this.load_wasm = async function(path) {
-    var data, env, result, typedArray;
-    env = {
-      memoryBase: 0,
-      tableBase: 0,
-      memory: new WebAssembly.Memory({
-        initial: 256
-      }),
-      table: new WebAssembly.Table({
-        initial: 0,
-        element: 'anyfunc'
-      })
-    };
-    data = (await FS.readFile(path));
-    typedArray = new Uint8Array(data);
-    result = (await WebAssembly.instantiate(typedArray)); //, env.memory
-    // result      = await WebAssembly.instantiate typedArray, { env, }
-    // result  = await WebAssembly.instantiateStreaming data
-    result.instance.exports.memory.grow(400); // each page is 64kb in size
-    return result.instance;
-  };
-
   //###########################################################################################################
   if (module === require.main) {
     (() => {
--- demo-nodejs-using-wasm/src/main.coffee
+++ demo-nodejs-using-wasm/src/main.coffee
@@ -20,21 +20,6 @@ FS                        = ( require 'fs' ).promises
 PATH                      = require 'path'
 # { promisify }
 
-#-----------------------------------------------------------------------------------------------------------
-@load_wasm = ( path ) ->
-  env =
-    memoryBase: 0,
-    tableBase: 0,
-    memory: new WebAssembly.Memory { initial: 256, }
-    table:  new WebAssembly.Table { initial: 0, element: 'anyfunc', }
-  data        = await FS.readFile path
-  typedArray  = new Uint8Array data
-  result      = await WebAssembly.instantiate typedArray #, env.memory
-  # result      = await WebAssembly.instantiate typedArray, { env, }
-  # result  = await WebAssembly.instantiateStreaming data
-  result.instance.exports.memory.grow 400 # each page is 64kb in size
-  return result.instance
-
 
 ############################################################################################################
 if module is require.main then do =>
