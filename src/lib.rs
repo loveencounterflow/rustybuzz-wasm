1bd7ed11a4640bceec48b083fa1b9141abbf9db9
flow <flow@enceladus.orbit>
2021-02-11 16:19:46 +0100

    implement default values in copy of `cfg`, without options

--- src/lib.rs
+++ src/lib.rs
@@ -20,46 +20,66 @@ extern {
 #[derive(Serialize, Deserialize)]
 // acc to https://doc.rust-lang.org/std/fmt/trait.Debug.html
 #[derive(Debug)]
-pub struct Cfg {
-    pub help: bool,
-    // pub text: Option<String>,
-    pub text: String,
+pub struct CfgOpt {
+    pub help: Option<bool>,
+    pub text: Option<String>,
     pub perhaps: Option<u32>,
     // pub perhaps: u32,
 }
 
-impl Cfg {
-    pub fn to_string( self: &Self  ) -> String {
-        return format!("{:#?}", self );
-    }
-    pub fn show_details(self: &Self) {
-        println!("help: {}", self.help);
-        println!("text: {:?}", self.text);
-        // println!("The employee name is {}", self.emp_name);
-        }
+// #[derive(Serialize, Deserialize)]
+#[derive(Debug)]
+pub struct Cfg {
+    pub help: bool,
+    pub text: String,
+    pub perhaps: u32,
+    // pub perhaps: u32,
 }
 
+// impl CfgOpt {
+//     pub fn to_string( self: &Self  ) -> String {
+//         return format!("{:#?}", self );
+//     }
+//     pub fn show_details(self: &Self) {
+//         println!("help: {}", self.help);
+//         println!("text: {:?}", self.text);
+//         // println!("The employee name is {}", self.emp_name);
+//         }
+// }
 
 
-
+//----------------------------------------------------------------------------------------------------------
 #[wasm_bindgen]
 pub fn greet( user_cfg: &JsValue ) {
     // // alert( &format!("{}", alert ) );
     // alert( &format!("Hello, {}!", name ) );
     //......................................................................................................
-    let cfg = Cfg {
-        help: true,
-        text: String::from( "some text" ),
-        // extra: 108,
-        perhaps: Some( 111 ),
-    };
-    info( &format!( "^34553^ help: {:#?}", cfg ) );
+    // let cfg_1 = CfgOpt {
+    //     help: true,
+    //     text: String::from( "some text" ),
+    //     // extra: 108,
+    //     perhaps: Some( 111 ),
+    // };
+    // info( &format!( "^34553^ help: {:#?}", cfg_1 ) );
     //......................................................................................................
     help( &format!( "^4575^ {:#?}", &user_cfg ) );
+    demo_options();
     //......................................................................................................
-    let elements: Cfg = user_cfg.into_serde().unwrap();
-    help( &format!( "^4575^ {:#?}", elements.text ) );
-    urge( &format!( "^4575^ {:#?}", elements ) );
+    let cfg_opt: CfgOpt = user_cfg.into_serde().unwrap();
+    let cfg = Cfg {
+        help: match cfg_opt.help { None => false, Some( x ) => x, },
+        text: match cfg_opt.text { None => String::from( "some text" ), Some( x ) => x, },
+        perhaps: match cfg_opt.perhaps { None => 42, Some( x ) => x, },
+
+    };
+    // cfg_opt.text = match cfg_opt.text {
+    //     None            => String::from( "default text" ),
+    //     Some( ref x )   => x.to_string(),
+    // };
+    // let mut cfg: Cfg;
+    // // cfg.perhaps = cfg.perhaps.unwrap();
+    // help( &format!( "^4575^ {:#?}", cfg.text ) );
+    urge( &format!( "^4575^ {:#?}", cfg ) );
     // help( &format!( "^4575^ {:#?}", &user_cfg ) );
     // help( &cfg.to_string() );
     // info( &format!( "^34553^ help: {:?}", cfg.help ));
@@ -73,3 +93,53 @@ pub fn greet( user_cfg: &JsValue ) {
 //   // shape::shape_text();
 //   return format!( "Hello, {}!", text );
 // }
+
+
+//##########################################################################################################
+struct FullNameOption {
+    first: String,
+    middle: Option<String>,
+    last: String,
+}
+
+struct FullName {
+    first: String,
+    middle: String,
+    last: String,
+}
+
+
+fn demo_options() {
+    let alice = FullNameOption {
+        first: String::from("Alice"),
+        middle: Some(String::from("Bob")), // Alice has a middle name
+        last: String::from("Smith")
+    };
+
+    let jon = FullNameOption {
+        first: String::from("Jon"),
+        middle: None, // Jon has no middle name
+        last: String::from("Snow")
+    };
+    // let alice_middle    = alice.middle.unwrap();
+    // let jon_middle      = match jon.middle {
+    //     None => "No middle name!",
+    //     Some( ref x ) => x,
+    //     };
+    // help( &format!( "^129^ {} - {} - {}", alice.first, alice_middle, alice.last ) );
+    // help( &format!( "^129^ {} - {} - {}", alice.first, alice_middle, alice.last ) );
+    // help( &format!( "^129^ {} - {} - {}", jon.first,   jon_middle, jon.last ) );
+    // // help( format!( "Alice's middle name is {}", alice.middle.unwrap() ).as_ref() );
+    // help( &format!( "Jon's middle name is {}", jon_middle ) );
+    let alice_resolved = FullName {
+        first: alice.first,
+        middle: match alice.middle {
+            None => String::from( "No middle name!" ),
+            Some( x ) => x,
+            },
+        last: alice.last,
+
+    };
+    help( &format!( "^129^ {} - {} - {}", alice_resolved.first, alice_resolved.middle, alice_resolved.last ) );
+}
+
