c82e4bd707a4a6b437d87bf248147af14ed55e20
flow <flow@enceladus.orbit>
2021-02-14 21:33:02 +0100

    update

--- demo-nodejs-using-wasm/lib/main.js
+++ demo-nodejs-using-wasm/lib/main.js
@@ -35,7 +35,7 @@
   if (module === require.main) {
     (() => {
       /* NOTE only works with `wasm-pack build --target nodejs` */
-      var RBW, cfg, font_bytes, font_bytes_hex, font_path, format, i, len, shy, text, texts;
+      var RBW, arrangement, cfg, d, font_bytes, font_bytes_hex, font_path, format, gid, gids, i, len, outline, ref, shy, text, texts;
       globalThis.alert = alert;
       globalThis.help = help;
       globalThis.urge = urge;
@@ -58,17 +58,54 @@
       }
       // font_bytes_hex      = 'abcdefgh'
       // format              = 'short'
-      // format              = 'json'
-      format = 'rusty';
+      format = 'json';
+      // format              = 'rusty'
       shy = '\xad';
-      texts = ["affix", "af#fix", " ", "#", "-"];
-      for (i = 0, len = texts.length; i < len; i++) {
-        text = texts[i];
-        text = text.replace(/#/g, shy);
-        cfg = {format, text};
-        info('^223^', RBW.shape_text(cfg));
+      // "a"
+      texts = ["affix"];
+      // "af#fix"
+      // " "
+      // "#"
+      // "-"
+      echo(`<?xml version='1.0' encoding='UTF-8'?>
+<svg xmlns='http://www.w3.org/2000/svg' width='6000' height='3000' viewBox='-100 -1500 5900 1500' version='2'>`);
+      // for text in texts
+      text = texts[0];
+      text = text.replace(/#/g, shy);
+      cfg = {format, text};
+      arrangement = JSON.parse(RBW.shape_text(cfg));
+      gids = new Set((function() {
+        var i, len, results;
+        results = [];
+        for (i = 0, len = arrangement.length; i < len; i++) {
+          d = arrangement[i];
+          results.push(d.gid);
+        }
+        return results;
+      })());
+      debug('^3344^', gids);
+      //.........................................................................................................
+      echo("<defs>");
+      ref = gids.values();
+      for (gid of ref) {
+        outline = JSON.parse(RBW.glyph_to_svg_pathdata(gid));
+        debug('^3344^', gid, outline);
+        // continue if outline.pd is ''
+        echo(`<symbol overflow='visible' id='g${gid}'>`);
+        echo(`<path d='${outline.pd}' transform='move(${d.x},${d.y + 1000})'/>`);
+        echo("</symbol>");
       }
-      urge('^690^', RBW.glyph_to_svg_pathdata(42));
+      echo("</defs>");
+//.........................................................................................................
+      for (i = 0, len = arrangement.length; i < len; i++) {
+        d = arrangement[i];
+        echo(`<use href='#g${d.gid}' x='${d.x}' y='${d.y}'/>`);
+      }
+      // echo "<g x='#{d.x}' y='#{d.y + 1000}'>"
+      // echo "#{outline.br}"
+      // echo "</g>"
+      //.........................................................................................................
+      echo("</svg>");
       return null;
     })();
   }
